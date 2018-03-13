357a11eb3fe32ac93649739c886e4339bd45f0db
tiptop25 <melnik_af@mail.ru>
2018-03-14 00:43:07 +0200

    added changes to Task1

--- src/test/java/Task1.java
+++ src/test/java/Task1.java
@@ -2,19 +2,56 @@
  * Created by superova on 13.03.2018.
  */
 
+import com.codeborne.selenide.Condition;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.open;
+import java.util.Random;
 
-public class Task1  {
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Task1 extends TestBase {
+    Random random = new Random();
+    String BASE = "https://www.centralpoint.nl";
+    String itemsString = ".card.landscape.wide";
+    String titleOfPage = ".title > h1";
+    String zoekenButton = ".filter.active .filterFooter > button";
+
+    public String setRandomNumber(){
+        String priceRangeLowString = $$("#priceRangeLow").last().attr("value");
+        String priceRangeHighString = $$("#priceRangehigh").last().attr("value");
+        int priceRangeLowInt = Integer.parseInt(priceRangeLowString);
+        int priceRangeHighInt = Integer.parseInt(priceRangeHighString);
+        System.out.println(priceRangeLowInt);
+        System.out.println(priceRangeHighInt);
+        int randomNumber = priceRangeLowInt + (int) (Math.random() * priceRangeHighInt);
+        String randomString = String.valueOf(randomNumber);
+        return randomString;
+    }
 
     @Test
     public void test1(){
-        open("https://www.centralpoint.nl/notebooks-laptops/");
+        open(BASE+"/notebooks-laptops/");
         $$(".mobileSwitchFiltersOff .filterHead").first().click();
         $$(".mobileSwitchFiltersOff .filterHead").shouldHaveSize(18);
-        //
+        $$("#priceRangeLow").last().val(setRandomNumber());
+        System.out.println(setRandomNumber());
+        $(zoekenButton).click();
+        $(titleOfPage).waitUntil(not(text("\n" +
+                "                                    Notebooks/laptops\n" +
+                "            \n" +
+                "                        (2060 resultaten)\n" +
+                "          ")),10000);
+    }
+
+    @Test
+    public void test2(){
+        String resultOfItems = $(titleOfPage).getText()
+                .replace("Notebooks/laptops (","")
+                .replace(" resultaten)","").trim();
+        int resultOfItemsInt = Integer.parseInt(resultOfItems);
+
     }
 
 }
